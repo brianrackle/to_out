c046ba2d3af5bce81f5ca9660ecebfe5409de40f
Brian Rackle <brianrackle@hotmail.com>
2014-12-10 23:03:11 -0800

    (broken) adding variable templates for parameter expansion

--- to_out.hpp
+++ to_out.hpp
@@ -1,32 +1,43 @@
 #include <type_traits>
 
-//TODO: add arguement expansion using parameter pack
-template<class T> struct member_function_traits;
-template<class T, class R, class A>
-struct member_function_traits<R (T::*)(A)>
+using namespace t_o
 {
-  using class_type = T;
-  using return_type = R;
-  using arg_type = A;
-};
+  template<class T> struct member_function_traits;
+  template<class T, class R, class... A>
+  struct member_function_traits<R (T::*)(A...)>
+  {
+    using class_type = T;
+    using return_type = R;
 
-//TODO: use variable templates for multiple parameter single return
-//TODO: allow multiple outputs to turn into tuple return type
-template<class O, class F, class P>
-auto to_out(O & obj, F func) -> typename std::remove_reference<P>::type
-{
-  typename std::remove_reference<P>::type out;
-  (obj.*func)(out);
-  return std::move(out);
-};
+    static constexpr std::size_t arity = sizeof...(A); //mathematics term for parameter count to function
+    static constexpr std::size_t max_arg = sizeof...(A) - 1; //will fail for parameter count == 0
 
-template<class O, class F, class P>
-P to_out(O && obj, F func)
-{
-  P out;
-  (obj.*func)(out);
-  return std::move(out);
-};
+    template <std::size_t N>
+    struct arg
+    {
+      using type = typename std::tuple_element<N, std::tuple<A...>>::type;
+    };
+  };
 
-//TODO: redefine t_o without a macro
-#define t_o(O, F) to_out<decltype(O), decltype(F), member_function_traits<decltype(F)>::arg_type>(O, F)
+  template class<T> using mft = member_function_traits<T>;
+  template class<T> using rr_t = std::remove_reference<T>::type;
+
+  //TODO(brian): allow variable parameters in to_out 
+  template<class O, class F>
+  auto to_out(O & obj, F func)
+  {
+    using mft_t = mft<F>;
+    typename rr_t<mft_t::arg<mft_t::max_arg> out; //get last parameter and remove the reference from it
+    (obj.*func)(out);
+    return std::move(out);
+  };
+
+  //  //explicit templating specifies this as r-value reference only
+  //  template<class O, class F>
+  //  P to_out(O && obj, F func)
+  //  {
+  //    P out;
+  //    (obj.*func)(out);
+  //    return std::move(out);
+  //  };
+}
